1a1119f17f4ebe3d35af730acdcc13446b274169
Rajesh Kumar <rajesh.kumar@cred.club>
2021-06-05 10:16:58 +0530

    initial commit

---
+++ src/test/java/com/rajitblog/interviewquestion/InterviewQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajitblog.interviewquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewQuestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
